608cd8c45f554f30c21d16bd4a451865ca188e5b
pth1080 <halleyphuong0108@gmail.com>
2020-10-06 15:29:29 +0700

    [TICKET_ID] Hello Git 3

--- hello_git_3.py
+++ hello_git_3.py
@@ -1,7 +1,11 @@
-def print_hello():
-    print('Hi')
+from hello_git import print_hello
+
+
+def print_sum(a: int, b: int):
+    print(a + b)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hello()
+    print_sum(5, 6)
